44eb9b1434919eef8d8f4a5ce18e0fdcac573802
APaud3l <aamod.paudel@coderacademy.edu.au>
2025-11-12 20:08:38 +1100

    API request on Pokemon API

--- index.js
+++ index.js
@@ -72,14 +72,14 @@ function getLiveJoke() {
 
 // Function written using async/await
 async function getJoke() {
-    try{
-    // request from the api
-    response = await fetch('https://official-joke-api.appspot.com/random_joke')
-    // convert the response to JSON
-    data = await response.json();
-    // print it in console
-    console.log("Joke: ", data.setup);
-    console.log("Punchline: ", data.punchline);
+    try {
+        // request from the api
+        response = await fetch('https://official-joke-api.appspot.com/random_joke')
+        // convert the response to JSON
+        data = await response.json();
+        // print it in console
+        console.log("Joke: ", data.setup);
+        console.log("Punchline: ", data.punchline);
     } catch (error) {
         console.error("Error: ", error);
     }
@@ -87,3 +87,73 @@ async function getJoke() {
 
 getJoke();
 console.log("I am running!!!");
+
+async function getPokemonData() {
+    const name = document.getElementById('pokemonName').value.toLowerCase();
+    console.log(name);
+    const resultDiv = document.getElementById('result');
+
+    try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+        if (!response.ok) {
+            console.error("Some error from the server!");
+        }
+        const data = await response.json();
+        console.log(JSON.stringify(data, null, 4));
+
+        const imgSrc = data.sprites.front_default;
+
+        resultDiv.innerHTML =
+            `
+        <h2>${data.name.toUpperCase()}</h2>
+        <img src="${imgSrc}" alt="${data.name}">
+        `;
+    } catch (error) {
+        console.error("Something happened.");
+        resultDiv.innerHTML =
+            `
+        <h2>${error.message}</h2>
+        `;
+    }
+}
+
+document.getElementById('catchPokemon').addEventListener('click', async () => {
+    getPokemonData();
+});
+
+document.getElementById('getRandomFive').addEventListener('click', async () => {
+    const resultDiv = document.getElementById('resultTeam');
+    resultDiv.innerHTML = "Loading 5 Pokemons for your team...";
+
+    // Get 5 random numbers
+    const maxID = 1025;
+
+    const randomIDs = Array.from({ length: 5 }, () => {
+        return (Math.ceil(Math.random() * maxID));
+    }
+
+    );
+    console.log("here?");
+    console.log(randomIDs);
+
+
+    // Fetch all pokemon in Parallel
+    const promises = randomIDs.map(id => {
+        return (fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+            .then(res => res.json()));
+    });
+    console.log(promises);
+    const pokemons = await Promise.all(promises);
+    console.log("How about here?");
+    console.log(pokemons);
+    // Build HTML element to display your Pokemon team
+    resultDiv.innerHTML = pokemons
+        .map(data => {
+            const imgSrc = data.sprites.front_default;
+
+            return `
+        <h2>${data.name.toUpperCase()}</h2>
+        <img src="${imgSrc}" alt="${data.name}">
+        `;
+        }).join('    ');
+});
